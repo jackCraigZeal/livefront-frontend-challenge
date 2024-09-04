0f40d584cd630624d3d7130a2b6ca8c8b5f4ef20
jackCraigZeal <jack.craig@zealitconsultants.com>
2024-09-04 15:28:19 -0500

    Created and styled a react component.

--- challenge-app/src/App.js
+++ challenge-app/src/App.js
@@ -4,22 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tutorials</h1>
+      <MyButton></MyButton>
     </div>
   );
+}
+
+function MyButton() {
+  return (
+    <>
+      <button className="fancyButton">I am a fancy button</button>
+      <button>I am an extra button</button>
+    </>
+  )
 }
 
 export default App;
